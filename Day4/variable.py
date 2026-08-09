af365aa3edee7be8d624630bd89dac22fdffc4aa
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-09 20:35:01 +0500

    Day 4 Python variables

---
+++ Day4/variable.py
@@ -0,0 +1,38 @@
+# c lang
+# int a = 4
+
+name = 'aliza'
+print(name)
+
+name = "sara"
+print(name)
+
+# no variable declaration
+# dynamic typing -->  
+# static typing
+
+print(name)
+
+int = 4
+print(int)
+
+name = True
+print(name)
+
+# dynamic binding 
+# static binding 
+# special syntax 
+a = 3 ; b = 4 ; c = 19 
+print(a)
+print(b)
+print(c)
+
+a,b,c = 11 , 12 , 19
+print(a)
+print(b)
+print(c)
+
+a = b = c = 19
+print(a)
+print(b)
+print(c)
