6b9ab031bf421b6a3dcedab82f02c7d91b41862a
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-09 12:20:24 +0500

    Day 02 - Variables and Data Types

---
+++ data type.py
@@ -0,0 +1,33 @@
+# integer
+print(3)
+print(1e309)
+
+# float 
+print(6.8)
+print(1.7e308)
+
+# boolean
+print(True)
+print(False)
+
+# complex
+print(3+5j)
+print(2+3j)
+
+# string 
+print("hellow")
+print('hellow')
+print('''hellow''')
+
+# list
+print([1,2,3,4,5])
+
+# tuple
+print((1,2,2,34,4,5,5,5,5,5,55,6))
+
+# sets
+print({3,4,5,6,7,8,9,9,0})
+
+# dict
+print({"name":"aliza","Age":20,"Gender":"female"})
+
