e80672ad5adfe81ddc8a4d4eaa9f8fc3506363be
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-11 11:53:53 +0500

    Fix Day 4 folder name

---
+++ Day3/variable.py
@@ -0,0 +1,35 @@
+# c lang 
+# int a = 5
+name = 'aliza'
+print(name)
+
+name = "sara"
+print(name)
+
+c = 'i love turkey'
+print(c)
+
+# no variable declaration
+# dynamic typing -->
+# static typing
+
+print(name)
+
+int = 4
+print(int)
+
+name = 'False'
+print(name)
+
+# dynamic binding
+# static binding
+# special syntax
+a = 4 ; b = 3 ; c = 8 
+print(a)
+print(b)
+print(c)
+
+a = b = c = 19
+print(a)
+print(b)
+print(c)
