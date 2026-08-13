8aaeeff42c62741c0ea0c4aa1c77298fc2ad40f6
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-13 11:43:28 +0500

    Day 06 - Type Conversion

---
+++ Day-06/type conversion.py
@@ -0,0 +1,34 @@
+# implicit
+# explicit 
+
+print(4 + 3.3) 
+
+print(4 + 5)
+
+print(5 + 6666)
+
+print(4 + 5454)
+
+print(19 + 11j)
+11
+print(4.5 + 5+5j)
+first_num = int(input("Enter first number: "))
+second_num = int(input("Enter second number: "))
+
+print(first_num + second_num)
+
+# int
+int('45')
+
+str(6)
+
+bool(1)
+
+complex(4)
+
+list('hellow')
+
+a = 4.76
+int(a)
+
+
