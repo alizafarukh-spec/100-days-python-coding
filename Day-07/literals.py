120d00960512dcecce070b09d1e7409d681df02f
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-17 12:42:33 +0500

    Add Day-07 literals

---
+++ Day-07/literals.py
@@ -0,0 +1,36 @@
+# numerical literal
+a = 0b1110 #binary literal
+b = 99 #decimal literal
+c = 0o310 #octal literal
+d = 0x12c #hexadecimal literal
+
+# float literal
+float_1 = 10.5
+float_2 = 1.2e2
+float_3 = 1.5e-3 
+
+# complex literal
+x = 3.14j
+
+print(a, b, c, d)
+print(float_1, float_2, float_3)
+print(x)
+
+# string literal
+string = "this is python"
+strings = "this is python"
+char = "C"
+multiline_str = """this is a multiline string with more than one line code."""
+
+print(string)
+print(strings)
+print(char)
+print(multiline_str)
+
+# boolean literal
+a = True + 4
+b = False + 10
+
+print("a:", a)
+print("b:", b)
+ 
