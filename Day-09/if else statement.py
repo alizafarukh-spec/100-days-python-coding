46a96c23193b10c2e5060270a7516ec6bbaaa504
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-19 11:48:26 +0500

    Added Day 09

---
+++ Day-09/if else statement.py
@@ -0,0 +1,61 @@
+# Program 1: Check whether a number is positive or negative
+
+num = int(input("Enter a number: "))
+
+if num >= 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
+
+
+
+   # Program 2: Check whether a number is even or odd
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+
+
+# Program 3: Check whether a person is eligible to vote
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+
+ # Program 4: Check which number is greater
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater")
+else:
+    print("Second number is greater")
+
+
+ # Program 5: Check pass or fail
+
+marks = int(input("Enter your marks: "))
+
+if marks >= 50:
+    print("You passed")
+else:
+    print("You failed")
+
+
+ # Program 6: Check whether a number is zero or not
+
+num = int(input("Enter a number: "))
+
+if num == 0:
+    print("The number is zero")
+else:
+    print("The number is not zero")
