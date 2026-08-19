cad2489cb74a3bb65c32020b6d65a36925347ec6
alizafarukh-spec <alizafarukh4@gmail.com>
2026-08-19 11:31:39 +0500

    Added Day 08 operators

---
+++ Day-08/operators.py
@@ -0,0 +1,72 @@
+x = 2
+y = 4
+print(x-y)
+
+print(x+y)
+
+print(x*y)
+
+print(x**y)
+
+print(x // 2)
+
+print(x%y)
+
+print(x > y)
+
+print(x < y)
+
+print(x == y)
+
+print(x != y)
+
+# logical operators 
+x = True
+y = False
+
+print(x or y)
+
+print(x and y)
+
+print(not y)
+
+# bitwise operator
+x = 4
+y = 6
+
+print(x & y)
+
+print(x | y)
+
+print(x >> 2)
+
+print(y << 3)
+
+# assignment 
+a = 3
+print(a)
+
+a += 3
+# a = a
+print(a)
+
+a -= 3
+a *= 4
+a &= 5
+
+a = 5 
+b = 4
+
+print(a is b)
+
+a = "hellow"
+b = "turkey"
+
+print(a is b)
+
+x = "dubai"
+print("D" not in x)
+x = [1,2,5]
+print(1 in x)
+
+
